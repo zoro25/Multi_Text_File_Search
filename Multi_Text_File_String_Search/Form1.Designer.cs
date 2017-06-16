7cb748ad2bef0d6929840655e688b3fcd4f9c8f8
Martin Hall (SKYPE) <martinha@microsoft.com>
2017-06-16 13:16:22 -0700

    FIrst Commit

---
+++ Multi_Text_File_String_Search/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Multi_Text_File_String_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1333, 974);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(8, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1317, 927);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Set Up";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(678, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File types to search within, Pipe seperator";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Text you with to search for within the files";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(683, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(448, 31);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "*.xml |*.txt |*.rft|*.bat";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(338, 345);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(634, 31);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Directory that will be searched - Network mapped drives are okay";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button2.Location = new System.Drawing.Point(175, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(448, 184);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DIrectory to search";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Text to Search?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.Location = new System.Drawing.Point(683, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(448, 184);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(8, 39);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1317, 927);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Results";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Files that will be searched for the string",
+            "Please choose a directory to populate this list. ",
+            "",
+            "Once the list is populated the \"Start Search Button\" will become active."});
+            this.listBox1.Location = new System.Drawing.Point(3, 670);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1311, 254);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1311, 921);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 974);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Muti-File String Search any questions martin@hb-help.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Multi_Text_File_String_Search/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_Text_File_String_Search
+{
+    public partial class Form1 : Form
+    {
+        public static string filetoload = String.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox2.Text = folderBrowserDialog1.SelectedPath.ToString();
+            button1.Enabled = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (textBox1.Text == @"Text to Search?")
+            {
+                MessageBox.Show("Please change the text to be searched");
+                return;
+            }
+                DirectoryInfo dinfo = new DirectoryInfo(textBox2.Text);
+               
+                Regex FileRegex = new Regex(@"\*\.\w\w\w");
+            FileInfo[] Files;
+            foreach (Match match in FileRegex.Matches(textBox3.Text))
+            {
+                Files = dinfo.GetFiles(match.Value);
+            
+            tabControl1.SelectTab(1);
+
+            
+            foreach (FileInfo file in Files)
+            {
+                listBox1.Items.Add(file.Name);
+               // listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                filetoload = textBox2.Text + "\\" + file.Name;
+                richTextBox1.LoadFile(filetoload, RichTextBoxStreamType.PlainText);
+                int indexTotext =  richTextBox1.Find(textBox1.Text);
+                if (indexTotext >= 0)
+                {
+                    MessageBox.Show(@"Found an occurance of the string you are looking for in " + file.Name);
+                    richTextBox1.SelectionColor = Color.White;
+                    richTextBox1.SelectionBackColor = Color.Black;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filetoload = textBox2.Text + @"\" + listBox1.SelectedItem;
+            richTextBox1.LoadFile(filetoload, RichTextBoxStreamType.PlainText);
+            int indexTotext = richTextBox1.Find(textBox1.Text);
+            if (indexTotext >= 0)
+            {
+                richTextBox1.SelectionStart = indexTotext;
+                int wordlength = textBox2.Text.Length;
+                richTextBox1.SelectionColor = Color.White;
+                richTextBox1.SelectionBackColor = Color.Black;
+
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
